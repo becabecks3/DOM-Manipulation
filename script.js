440588dc92e83eac80909c4187e0fc8f65fdc452
Rebeca <becabecks3.gmail.com>
2023-05-04 09:47:09 +0200

    changes-made

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//CREAR LI
 let nLi = document.createElement('li');
 let txt = document.createTextNode('Esto es un Li');
 nLi.appendChild(txt);
@@ -8,29 +9,31 @@ let txt2 = document.createTextNode('Esto es un Li');
 nLi2.appendChild(txt2);
 document.querySelector('.uList').appendChild(nLi2);
 
+//CREAR PARRAFO DEBAJO DEL BOTON, ESCONDERLO Y MOSTRARLO
 let nP = document.createElement('p');
 let txtP = document.createTextNode('Ahora me ves');
 nP.appendChild(txtP);
 nP.setAttribute("id","para");
 document.querySelector('.hide').appendChild(nP);
 
-let txt4 = document.querySelector('#para');
+let txt3 = document.querySelector('#para');
 let button = document.querySelector('.button');
 button.addEventListener('click', function() {
-    txt4.classList.toggle('para');
-})
+    txt3.classList.toggle('para');
+});
 
-let pic = document.createElement('h2');
-let txt3 = document.createTextNode('Capibara, Capibara!');
-pic.appendChild(txt3);
-document.querySelector('.capibara').appendChild(pic);
+//CREAR H2 Y ESCONDERLO Y MOSTRARLO
+let title2 = document.createElement('h2');
+let txt4 = document.createTextNode('Capibara, Capibara!');
+title2.appendChild(txt4);
+document.querySelector('.capibara').appendChild(title2);
 
 let img = document.querySelector('.capibara img');
 img.addEventListener('mouseout', function() {
-    pic.style.visibility = 'hidden';
+    title2.style.visibility = 'hidden';
 });
 
 img.addEventListener('mouseenter', function() {
-    pic.style.visibility = 'visible';
+    title2.style.visibility = 'visible';
 });
 
